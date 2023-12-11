095497205d2998794ed339cd551c4d964eb39203
tmarr1426 <tmarr1426@gmail.com>
2023-12-11 17:03:59 -0600

    Started the project. Utilizing IF, Else IF, etc.

--- index.js
+++ index.js
@@ -14,7 +14,33 @@ async function start() {
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
   // Now try and complete the program.
-  process.exit();
+  let answer = await ask("Is your number 50? (Y/N)\n");
+  if (answer == "Y") {
+    console.log(`Your number was ${secretNumber}!`);
+  } else if (answer == "N") {
+    let answer2 = await ask("Higher or Lower? (H/L)\n");
+    if (answer2 === "H") {
+      let answer3 = await ask("Is your number 75? (Y/N)\n");
+      if (answer3 === "Y") {
+        console.log(`Your number was ${secretNumber}!`);
+      } else if (answer3 == "N"){
+        let answer4 = await ask("Higher or Lower? (H/L)\n");
+        if (answer4 ="H") {
+          let answer6 = await ask("Is your number 87 (Y/N) \n")
+        } else if (answer4 === L) {
+          let answer8 = await ask("Higher or Lower (H/L) \n");
+        }
+      }
+    } else {
+      if (answer2 == "L") {
+        let answer5 = await ask("Is your number 25? (Y/N) \n")
+      } 
+    }
+  } else {
+    console.log("ERROR please answer Y or N. \n")
+    //process.exit();
+  };
 }
 
-console.log("test")
+start();//console.log("test")
+

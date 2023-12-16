de7a58a45597ffdbace18228d51a4bfe71295abd
tmarr1426 <tmarr1426@gmail.com>
2023-12-15 18:20:09 -0600

    testing

--- index.js
+++ index.js
@@ -14,20 +14,25 @@ async function start() {
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
   // Now try and complete the program.
+
   var minNum = 1; // sets a minimum
   var maxNum = 100 // sets a maximum
   var guess; // starts the guessing
+  let attempts = 0;
+
   do { // Do While loop that alters the high and low end of the variables to 
     var guess = Math.floor((maxNum-minNum) / 2) + 1; // Makes an estimated guess based off of max and min.
+    attempts ++
     let answer = await ask (`Is your secret number ${guess}? (Y/N)\n`); //Checks to see if the computers guess is equal to secret number
+    console.log(minNum, maxNum); // Test line
     if (answer === "N") { // User response
       let answer2 = await ask (`Higher or Lower (H/L)\n`) // Computer asks if the secret number is higher or lower than their guess.
-      if (answer2 === "H") { // User response
-        var minNum = guess; // Sets new minimum = to the guessed amount EG: guess was 50 new min is 50.
-        var maxNum = 100;
-      } else if (answer2 === "L"){ // User response
-        var minNum = 1; 
+      if (answer2 === "H") {// User response
+        minNum = guess; // Sets new minimum = to the guessed amount EG: guess was 50 new min is 50.
+        maxNum;
+      }  else if (answer2 === "L") { 
         var maxNum = guess; // Sets a new maximum = the guessed amount EG: guess was 50 new max is 50.
+        var minNum; 
       }
     } else if (answer === "Y"){ // User response
       console.log(`Woohoo, your secret number was ${guess}!`)
--- reverse_game.js
+++ reverse_game.js
@@ -7,20 +7,30 @@ function ask(questionText) {
   });
 }
 
-async function guessNumber() {
-    const secretNumber = Math.floor(Math.random() * 10) + 1;
+async function reverse_start() {
+    const secretNumber = Math.floor(Math.random() * 100) + 1;
     let guess;
+    let attempts = 0;
     
-    while (true) {
-      guess = prompt('Guess a number between 1 and 10:');
-      
+    do {
+      let guess = await ask('Guess a number between 1 and 100. \n');
+      attempts ++
+
       if (guess === secretNumber) {
-        alert('Congratulations! You guessed the correct number.');
+        console.log('Congratulations! You guessed the correct number.');
         break;
-      } else {
-        alert('Try again!');
+      } else  if (guess > secretNumber) {
+        console.log('Try Lower');
+      } else if (guess < secretNumber) {
+        console.log(`Try Higher`);
       }
     }
+    while (guess != secretNumber) {
+      let end = await ask (`Thanks for playing! Do you want to play again? (Y/N)?\n`)
+      if (end === "Y") {
+    } else if (end === "N") { // User response
+      console.log("That was fun! Bye!");
+    }
   }
-  
-  guessNumber();
+}  
+reverse_start();

602351ef677587dc12a33a2a5bcb43083339742e
tmarr1426 <tmarr1426@gmail.com>
2023-12-15 22:40:46 -0600

    FINISHED THE MAIN GAME! WOOHOO!

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function start() {
     if (answer === "N") { // User response
       let answer2 = await ask (`Higher or Lower (H/L)\n`) // Computer asks if the secret number is higher or lower than their guess.
       if (answer2 === "H") {// User response
-        minNum = guess; // Sets new minimum = to the guessed amount EG: guess was 50 new min is 50.
+        minNum = guess + 1; // Sets new minimum = to the guessed amount EG: guess was 50 new min is 50.
         maxNum;
       }  else if (answer2 === "L") { 
         var maxNum = guess; // Sets a new maximum = the guessed amount EG: guess was 50 new max is 50.

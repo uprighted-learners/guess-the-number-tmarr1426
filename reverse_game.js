746cc6f182829a9941955b8f7e55f74bee61dbbf
tmarr1426 <tmarr1426@gmail.com>
2023-12-17 07:40:35 -0600

    Put both the Reverse Game and the main game together. Have to add cheat detector to main game.

--- reverse_game.js
+++ reverse_game.js
@@ -8,29 +8,27 @@ function ask(questionText) {
 }
 
 async function reverse_start() {
-    const secretNumber = Math.floor(Math.random() * 100) + 1;
-    let guess;
-    let attempts = 0;
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
+  let guess;
+  let attempts = 0;
     
-    do {
-      let guess = await ask('Guess a number between 1 and 100. \n');
-      attempts ++;
+  while (guess != secretNumber) {
+    let guess = await ask('Guess a number between 1 and 100. \n');
+    attempts ++;
 
-      if (guess === secretNumber) {
-        console.log(`Congratulations! You guessed the correct number. It took you ${attempts} attempts!`);
-        break;
-      } else  if (guess > secretNumber) {
-        console.log('Try Lower');
-      } else if (guess < secretNumber) {
-        console.log(`Try Higher`);
-      }
-    }
-    while (guess != secretNumber) {
+    if (guess = secretNumber) {
+      console.log(`Congratulations! You guessed the correct number. It took you ${attempts} attempts!`);
       let end = await ask (`Thanks for playing! Do you want to play again? (Y/N)?\n`)
       if (end === "Y") {
-    } else if (end === "N") { // User response
+        reverse_start();
+      } else if (end === "N") { // User response
       console.log("That was fun! Bye!");
       process.exit();
+        }
+    } else  if (guess > secretNumber) {
+        console.log('Try Lower');
+    } else if (guess < secretNumber) {
+        console.log(`Try Higher`);
     }
   }
 }  

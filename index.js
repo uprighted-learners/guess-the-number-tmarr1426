c4e394900fb9a3563ba329edf6755b4c043fd936
tmarr1426 <tmarr1426@gmail.com>
2023-12-19 07:37:42 -0600

    Finalizing Cheating Errors, then done

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function ask(questionText) {
   });
 }
 
+const randomGuesser = (min, max) => {
+  return Math.floor((min+max) / 2);
+}
 
 async function start() {
   let game = await ask("Let's play a game! Please choose either normal or reverse. (N/R)\n")
@@ -30,24 +33,28 @@ async function start() {
     let attempts = 0;
 
     while (guess != secretNumber){
-      var guess = Math.floor((maxNum+minNum) / 2) ; // Makes an estimated guess based off of max and min.
+      var guess = randomGuesser(minNum, maxNum); // Makes an estimated guess based off of max and min.
       attempts ++;
       let answer = await ask (`Is your secret number ${guess}? (Y/N)\n`); //Checks to see if the computers guess is equal to secret number
       console.log("Attempt Number:", attempts, "Minimum Value:", minNum, "Maximum Value:", maxNum, "secret number:", secretNumber);
       if (answer === "N") { // User response
         let answer2 = await ask (`Higher or Lower (H/L)\n`) // Computer asks if the secret number is higher or lower than their guess.
         if (answer2 === "H") {// User response
-          minNum = guess + 1; // Sets new minimum = to the guessed amount EG: guess was 50 new min is 50.
-          maxNum;
-          // if (guess > maxNum) {
+          if (minNum > maxNum) {
 
-          //   let answer3 = await ask(`What do you mean Higher? I already guessed ${maxNum}! Would you like to restart the game? (Y/N)?\n`);
+            let answer3 = await ask(`What do you mean Higher? I already guessed ${maxNum}! Would you like to restart the game? (Y/N)?\n`);
+            if (answer3 === "Y") {
+              start();
+            } else if (answer3 === "N"){
+              console.log("There must have been an error. End sequence.");
+              process.exit();
+            }
 
-          //   start();
-
-          // } else {
-          //   return;
-          // }
+          } else {
+            minNum = guess + 1; // Sets new minimum = to the guessed amount EG: guess was 50 new min is 50.
+            maxNum;
+            //let answer2 = await ask (`Higher or Lower (H/L)\n`);
+          }
         }  else if (answer2 === "L") { 
           var maxNum = guess; // Sets a new maximum = the guessed amount EG: guess was 50 new max is 50.
           var minNum;
